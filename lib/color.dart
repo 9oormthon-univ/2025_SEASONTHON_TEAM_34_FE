5bd1d876b9e305d9f7e4f084e20687e46d3bf747
ohdoyoel <ohdoyoel@naver.com>
2025-09-07 05:15:22 +0900

    Add grayColor constant to color palette

--- lib/color.dart
+++ lib/color.dart
@@ -4,4 +4,5 @@ const Color primaryColor = Color(0xFF25BC52);
 const Color secondaryColor = Color(0xFFD4FFE0);
 const Color backgroundColor = Color(0xFFEDFFF2);
 const Color whiteColor = Color(0xFFFFFFFF);
+const Color grayColor = Color(0xFF888888);
 const Color blackColor = Color(0xFF000000);
